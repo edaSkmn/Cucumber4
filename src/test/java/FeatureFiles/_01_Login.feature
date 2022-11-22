7676171d29f1a90aab24c735eb0ff4d9e2a03de1
edaSkmn <edasekmen@windowslive.com>
2022-11-22 20:52:02 +0300

    Framework hazirlik 1

---
+++ src/test/java/FeatureFiles/_01_Login.feature
@@ -0,0 +1,13 @@
+#Senaryo:
+#1- siteyi aciniz
+# 2- username ve passwordu gonderin ve Login butonuna tiklatin.
+# 3- Login oldugunuzu dogrulayin.
+
+
+Feature: Login Functionality
+
+  Scenario: Login with valid username and password
+
+    Given Navigate to Campus
+    When Enter username and password and click Login button
+    Then User should login successfully
---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @When("Enter username and password and click Login button")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+    }
+
+    @Then("User should login successfully")
+    public void userShouldLoginSuccessfully() {
+    }
+}
