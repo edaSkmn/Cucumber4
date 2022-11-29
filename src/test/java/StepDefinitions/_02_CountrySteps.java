96237315c4945013d4b32fc06b723e808d44733a
edaSkmn <edasekmen@windowslive.com>
2022-11-29 22:14:34 +0300

    Framework hazırlık ve Country Create

--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -1,12 +1,15 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
 import Pages.LeftNav;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class _02_CountrySteps {
     LeftNav ln=new LeftNav();
+    DialogContent dc=new DialogContent();
     @And("Navigate to country page")
     public void navigateToCountryPage() {
         ln.findAndClick("setUpOne");
@@ -17,13 +20,29 @@ public class _02_CountrySteps {
 
     @When("Create a country")
     public void createACountry() {
-        //TODO: createCountry
+        String randomGenName= RandomStringUtils.randomAlphanumeric(8);
+        String randomGenCode=RandomStringUtils.randomNumeric(4);
+
+        dc.findAndClick("addButton");
+        dc.findAndSend("nameInput",randomGenName);
+        dc.findAndSend("codeInput",randomGenCode);
+        dc.findAndClick("saveButton");
 
     }
 
     @Then("Success message should be displayed")
     public void successMessageShouldBeDisplayed() {
-        //TODO: successMessageShouldBeDisplayed
+        dc.findAndContainsText("successMessage","success");
+
+
+    }
+
+    @When("Create a country name as {string} code as {string}")
+    public void createACountryNameAsCodeAs(String name, String code) {
+        dc.findAndClick("addButton");
+        dc.findAndSend("nameInput",name);
+        dc.findAndSend("codeInput",code);
+        dc.findAndClick("saveButton");
 
     }
 }
