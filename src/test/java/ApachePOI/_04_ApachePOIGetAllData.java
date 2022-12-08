d46a6390556986a1a290ee7eccab041d308b555d
edaSkmn <edasekmen@windowslive.com>
2022-12-08 21:56:10 +0300

    ApachePOI 1

---
+++ src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
@@ -0,0 +1,46 @@
+package ApachePOI;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class _04_ApachePOIGetAllData {
+    public static void main(String[] args) throws IOException {
+
+        String path = "src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
+        FileInputStream inputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        Sheet sheet=workbook.getSheetAt(0);
+
+        // calisma sayfasındaki fiziksel kullanılan toplam satır sayısını veriyor.
+        int rowCount= sheet.getPhysicalNumberOfRows(); // satir sayisini gosteriyor
+
+        for (int i = 0; i < rowCount; i++) {
+
+            Row row= sheet.getRow(i); // i. satir alindi
+
+            int cellCount= row.getPhysicalNumberOfCells(); // uzunluk alindi
+
+            for (int j = 0; j < cellCount; j++) {
+                Cell cell= row.getCell(j);
+                System.out.print(cell + "\t");
+
+            }
+            System.out.println();
+
+        }
+        //2.ci yöntem
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+
+            for (int j = 0; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+                System.out.print(sheet.getRow(i).getCell(j)+"\t");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
