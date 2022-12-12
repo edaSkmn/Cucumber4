71be60a16a0a0e27d73e08103242dcb9fe58d2b8
edaSkmn <edasekmen@windowslive.com>
2022-12-12 19:49:09 +0300

    ApachePOI 2 - MasterThought Parallel Rapor işlemleri

--- src/test/java/Runners/_11_TestParallel_MasterRep.java
+++ src/test/java/Runners/_11_TestParallel_MasterRep.java
@@ -1,19 +1,20 @@
 package Runners;
 
+
 import Utilities.GWD;
-import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 @CucumberOptions(
         tags = "@Regression",
-        features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        features = {"src/test/java/FeatureFiles/"},
+        glue = {"StepDefinitions"},
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        // master report için
 )
-public class _09_TestRunnerParallel extends AbstractTestNGCucumberTests {
+public class _11_TestParallel_MasterRep extends AbstractTestNGCucumberTests {
 
     @BeforeClass
     @Parameters("browser")
@@ -22,4 +23,5 @@ public class _09_TestRunnerParallel extends AbstractTestNGCucumberTests {
         // borwser türünü GWD ye gönder
         GWD.setThreadBrowserName(browser);
     }
+
 }
